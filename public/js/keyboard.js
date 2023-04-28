beac0bf65846a381b5508d4352377fdd71e13b00
Eugene Babenko <eababenko@gmail.com>
2023-04-28 13:17:47 +0300

    feat: add function for generating keyboard HTML code

--- public/js/keyboard.js
+++ public/js/keyboard.js
@@ -21,6 +21,8 @@ export class Keyboard {
   #init() {
     const elComp = utils.createElementWithAttributes('div', 'comp');     
     elComp.appendChild(this.#getMonitor());
+    elComp.appendChild(this.#getKeyboard());
+    
     this.#state.container.appendChild(elComp);
     console.log(this.#state);
   }
@@ -33,6 +35,53 @@ export class Keyboard {
     elMonitorScreen.appendChild(elWinEditor);
     elMonitor.appendChild(elMonitorScreen);
     return elMonitor;
+  }
+
+  #getKeyboard() {
+    const elKeyboard = utils.createElementWithAttributes('div', 'keyboard');
+       
+    this.#state.keybordInfo.keys.sort((a, b) => {
+      if (a.row === b.row) {
+        return a.pos - b.pos;
+      }
+      return a.row - b.row;
+    });
+
+    let curentRow = 0;
+    let elRowKeys;
+    
+    this.#state.keybordInfo.keys.forEach(btn => {           
+      if (curentRow !== btn.row) {
+        if (curentRow > 0) {
+          elKeyboard.appendChild(elRowKeys);
+        }
+        curentRow = btn.row;
+        elRowKeys = utils.createElementWithAttributes('div', 'keyboard__row');
+      }
+      const elKey = utils.createElementWithAttributes('button', 'keyboard__key', {'data-code': btn.code})
+      if (btn.level_1) {
+        elKey.textContent = btn.level_1;
+      }
+      if (btn.title) {
+        elKey.textContent = btn.title;
+      }
+      
+      if (btn.width) {
+        if (btn.width === "grow") {
+          elKey.classList.add('keyboard__key--grow');
+        } else {
+          elKey.setAttribute('style', `width: ${parseFloat(btn.width)}rem`);
+        }
+      }
+
+      elRowKeys.appendChild(elKey);
+    });
+    if (curentRow > 0) {
+      elKeyboard.appendChild(elRowKeys);
+    }
+    console.log(elKeyboard);
+
+    return elKeyboard;
   }
 
   #getKeyboardInfo(lang) {
