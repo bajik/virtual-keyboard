fa74985d9d0e5a50a79e478167cbc7fab1f08bf5
Eugene Babenko <eababenko@gmail.com>
2023-04-28 12:21:23 +0300

    feat: Add createElementWithAttributes function

---
+++ public/js/main.js
@@ -0,0 +1,3 @@
+import * as utils from './utils.js';
+
+console.log(utils.createElement('button', 'keyboard__key', {'data-code': 'ghgh'}));
---
+++ public/js/utils.js
@@ -0,0 +1,15 @@
+
+export function createElementWithAttributes(tag, clasNames, attributes) {
+  if (typeof(tag) !== 'string') return undefined;
+  const elem = document.createElement(tag);
+  
+  if (clasNames) {
+    elem.classList.add(...clasNames.split(' '));
+  }
+  if (attributes) {
+    for (const attr in attributes) {
+      elem.setAttribute(attr, attributes[attr]);
+    }
+  }
+  return elem;
+}
